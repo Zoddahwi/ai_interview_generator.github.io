ab56d59b8ec6b5dfa56141b057cc2d4d602d7cbe
zoddahwi <zoddahvictorwise@gmail.com>
2026-05-18 15:30:49 +0000

    Updates

---
+++ _next/static/DSthxPYFaCfi4MjK4qf9c/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ai_interview_generator.github.io//_next/:path+",
+        "destination": "/ai_interview_generator.github.io/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
